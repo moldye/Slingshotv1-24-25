cf9cd226f12741fb44b07faf7a811bcb81f673fe
yonnie <yonnie.g.yang@gmail.com>
2024-09-08 14:08:01 -0500

    starting autonomous pathing

--- MeepMeep/src/main/java/com/example/meepmeep/Tester.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Tester.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -19,16 +20,22 @@ public class Tester {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-60, -12, Math.toRadians(180)))
+                                .back(28)
+                                .splineToLinearHeading(new Pose2d(-30, -48, 0), Math.toRadians(0))
+                                .lineTo(new Vector2d(-55, -48))
+                                
+
+
+
+
                                 .build()
+
+
+
+
+
+
                 );
 
 
