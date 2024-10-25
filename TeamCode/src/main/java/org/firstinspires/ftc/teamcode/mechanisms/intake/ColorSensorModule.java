cd2b24a53ba6b01657edae5447af1ddb2a7f1ba4
Aubrey Tipps <aatipps@gmail.com>
2024-10-25 15:42:35 -0500

    no distance sensor color sensor, the rev color sensor can be color OR distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/intake/ColorSensorModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/intake/ColorSensorModule.java
@@ -10,15 +10,15 @@ import org.firstinspires.ftc.teamcode.mechanisms.intake.IntakeConstants.SampleTy
 
 public class ColorSensorModule {
     Telemetry telemetry;
-    ColorRangeSensor sensor;
+    ColorSensor sensor;
     public boolean isBlue;
     double blockDistance;
     //TODO:I'm too lazy to actually put this into intake, so I'm making a class for it HOORAY!!
     public ColorSensorModule(Telemetry t, HardwareMap hm, boolean isBlue){
         this.telemetry = t;
-        this.sensor = hm.get(ColorRangeSensor.class, "colorSensor");
+        this.sensor = hm.get(ColorSensor.class, "colorSensor");
         this.isBlue = isBlue;
-        blockDistance = sensor.getDistance(DistanceUnit.CM);
+        // blockDistance = sensor.getDistance(DistanceUnit.CM);
     }
     public SampleTypes checkSample(){
         double[] sensorVals = new double[3];
@@ -35,16 +35,16 @@ public class ColorSensorModule {
         }
         return best;
     }
-    public boolean hasPixel(){
-        if(blockDistance*0.95 > sensor.getDistance(DistanceUnit.CM)) {
-            //this code runs if the block is closer
-            return true;
-        } else if (blockDistance*1.05 > sensor.getDistance(DistanceUnit.CM)) {
-            //this code runs if the closest thing is further than the percieved block.
-            telemetry.addData("sensor greater diff", sensor.getDistance(DistanceUnit.CM)-blockDistance);
-        }
-        return false;
-    }
+//    public boolean hasPixel(){
+//        if(blockDistance*0.95 > sensor.getDistance(DistanceUnit.CM)) {
+//            //this code runs if the block is closer
+//            return true;
+//        } else if (blockDistance*1.05 > sensor.getDistance(DistanceUnit.CM)) {
+//            //this code runs if the closest thing is further than the percieved block.
+//            telemetry.addData("sensor greater diff", sensor.getDistance(DistanceUnit.CM)-blockDistance);
+//        }
+//        return false;
+//    }
     public boolean opposingColor(){
         double[] sensorVals = new double[3];
         sensorVals[0] = sensor.red();
