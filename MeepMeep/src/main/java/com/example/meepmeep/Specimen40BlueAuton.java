a99a341421e429ee362bcf4a3a32a2639cfa197e
yonnie <yonnie.g.yang@gmail.com>
2024-11-10 19:13:14 -0600

    made new path for spec (currently 3+0)

--- MeepMeep/src/main/java/com/example/meepmeep/Specimen40BlueAuton.java
+++ MeepMeep/src/main/java/com/example/meepmeep/Specimen40BlueAuton.java
@@ -1,6 +1,7 @@
 package com.example.meepmeep;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -25,61 +26,61 @@ public class Specimen40BlueAuton {
                                 //score specimen (preloaded)
 
                                 .back(36)
-//                                .waitSeconds(.5)
-//                                .forward(5)
-//
-//                                //go to HP
-//                                .setReversed(true)
-//                                .lineToSplineHeading(new Pose2d(36, -35, Math.toRadians(180)))
-//                                .splineTo(new Vector2d(60, -60 ), Math.toRadians(270))
-//                                .waitSeconds(.5)
-//
-//                                //go to box
-//                                .setReversed(false)
-//                                .splineTo(new Vector2d(36, -35), Math.toRadians(180))
-//                                .lineToSplineHeading(new Pose2d(9, -35, Math.toRadians(270)))
-//                                .back(5)
-//                                .waitSeconds(.5)
-//                                .forward(5)
-//
-//                                //go to HP
-//                                .setReversed(true)
-//                                .lineToSplineHeading(new Pose2d(36, -35, Math.toRadians(180)))
-//                                .splineTo(new Vector2d(60, -60 ), Math.toRadians(270))
-//                                .waitSeconds(.5)
-//
-//                                //go to box
-//                                .setReversed(false)
-//                                .splineTo(new Vector2d(36, -35), Math.toRadians(180))
-//                                .lineToSplineHeading(new Pose2d(6, -35, Math.toRadians(270)))
-//                                .back(5)
-//                                .waitSeconds(.5)
-//                                .forward(5)
-//
-//
-//
-//
-//
-//
-//                                //get sample
-//                                .splineToSplineHeading(new Pose2d(25,-50,Math.toRadians(-60)),Math.toRadians(0))
-//                                .lineToSplineHeading(new Pose2d(48, -50, Math.toRadians(90)))
-//                                .waitSeconds(0.5)
-//                                .turn(Math.toRadians(-40))
-//                                .waitSeconds(0.5)
-//                                .turn(Math.toRadians(40))
-//                                .waitSeconds(0.5)
-//                                .turn(Math.toRadians(-50))
+                                .waitSeconds(.5)
+                                .forward(5)
+
+                                //go to HP
+                                .setReversed(true)
+                                .lineToSplineHeading(new Pose2d(36, -35, Math.toRadians(180)))
+                                .splineTo(new Vector2d(60, -60 ), Math.toRadians(270))
+                                .waitSeconds(.5)
+
+                                //go to box
+                                .setReversed(false)
+                                .splineTo(new Vector2d(36, -35), Math.toRadians(180))
+                                .lineToSplineHeading(new Pose2d(9, -35, Math.toRadians(270)))
+                                .back(5)
+                                .waitSeconds(.5)
+                                .forward(5)
+
+                                //go to HP
+                                .setReversed(true)
+                                .lineToSplineHeading(new Pose2d(36, -35, Math.toRadians(180)))
+                                .splineTo(new Vector2d(60, -60 ), Math.toRadians(270))
+                                .waitSeconds(.5)
+
+                                //go to box
+                                .setReversed(false)
+                                .splineTo(new Vector2d(36, -35), Math.toRadians(180))
+                                .lineToSplineHeading(new Pose2d(6, -35, Math.toRadians(270)))
+                                .back(5)
+                                .waitSeconds(.5)
+                                .forward(5)
+
+
+
+
+
+
+                                //get sample
+                                .splineToSplineHeading(new Pose2d(25,-50,Math.toRadians(-60)),Math.toRadians(0))
+                                .lineToSplineHeading(new Pose2d(48, -50, Math.toRadians(90)))
+                                .waitSeconds(0.5)
+                                .turn(Math.toRadians(-40))
+                                .waitSeconds(0.5)
+                                .turn(Math.toRadians(40))
+                                .waitSeconds(0.5)
+                                .turn(Math.toRadians(-50))
+                                .waitSeconds(0.5)
+                                .turn(Math.toRadians(50))
+
+                                .back(10)
+
+
+
 //                                .waitSeconds(0.5)
 //                                .turn(Math.toRadians(50))
-//
-//                                .back(10)
-//
-//
-//
-////                                .waitSeconds(0.5)
-////                                .turn(Math.toRadians(50))
-//
+
 
                                 .build()
 
