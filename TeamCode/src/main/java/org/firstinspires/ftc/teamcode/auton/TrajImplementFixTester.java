739f2381d6b94d3d6e56b03a6c74479348bb9ce2
jihoo <0919imjihoon@gmail.com>
2024-11-27 02:46:37 -0600

    Roadrunner continuous and adaptive correcting branch

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/TrajImplementFixTester.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.mechanisms.intake.Intake;
+import org.firstinspires.ftc.teamcode.mechanisms.intake.IntakeConstants;
+import org.firstinspires.ftc.teamcode.mechanisms.outtake.Outtake;
+import org.firstinspires.ftc.teamcode.mechanisms.specimen.SpecimenClaw;
+import org.firstinspires.ftc.teamcode.misc.gamepad.GamepadMapping;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@TeleOp
+public class TrajImplementFixTester extends LinearOpMode {
+
+    private GamepadMapping controls;
+    private Robot robot;
+    private static IntakeConstants.ActiveIntakeStates activeIntakeStates;
+    private Intake intake;
+    private Outtake outtake;
+    private SpecimenClaw specimenClaw;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        controls = new GamepadMapping(gamepad1, gamepad2);
+        robot = new Robot(hardwareMap, telemetry, controls);
+        intake = robot.intake;
+        outtake = robot.outtake;
+        specimenClaw = robot.specimenClaw;
+        robot.outtake.resetEncoders();
+        moveLift(0);
+        outtake.bucketToReadyForTransfer();
+        intake.extendoFullRetract();
+        intake.activeIntake.flipUp();
+        specimenClaw.closeClaw();
+
+        //robot.hardwareHardReset();
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                .lineToConstantHeading(new Vector2d(0,30))
+                .waitSeconds(10)
+                .lineToConstantHeading(new Vector2d(0,0))
+                .build();
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(gamepad1.a){
+                if (!isStopRequested())
+                    drive.followTrajectorySequence(trajSeq);
+            }
+        }
+
+    }
+
+    public void moveLift(int ticks){
+        robot.outtake.outtakeSlideLeft.setTargetPosition(ticks);
+        robot.outtake.outtakeSlideRight.setTargetPosition(ticks);
+        robot.outtake.outtakeSlideLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.outtake.outtakeSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.outtake.outtakeSlideLeft.setPower(1);
+        robot.outtake.outtakeSlideRight.setPower(1);
+    }
+    public void moveExtendo(double pos){
+        robot.intake.leftExtendo.setPosition(pos);
+        robot.intake.rightExtendo.setPosition(pos);
+    }
+
+}
